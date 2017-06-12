8ca21ce2fdcbb581193974ba4d64ea022478592d
Richard Forrester <richard.forrester@gmail.com>
2017-06-12 04:38:37 +0000

    added feature product reviews and ratings

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,22 @@
 class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @review = @product.reviews.create(params[:review])
+    @review = @product.reviews.new(review_params)
+    @user = User.last
+    @review.user_id = @user.id
+
+
+if @review.save
+      redirect_to :back, notice: 'Review was successfully created.'
+    else
+      redirect_to :back, notice: 'Failed to create the review'
+    end
   end
 
-
- private
+  private
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:whale).permit(:awesome)
+      params.require(:review).permit(:description, :rating)
     end
-
 end
+
